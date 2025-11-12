771e0b7c21f0a335fddc3ad861fe5303c2087d2e
Bruno Daniel Afonso <brunoafonso@MacBook-Air-de-Bruno.local>
2025-11-12 10:37:54 -0300

    porta certa

--- server/index.ts
+++ server/index.ts
@@ -169,7 +169,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5001;
+  const port = 5000;
   log(`serving on port ${port}`);
   server.listen(port);
 })();
